f95c9716b7a46a19b63bf01456f0bb954d3870c2
Francisco Javier <fbottazzini@uade.edu.ar>
2025-05-13 18:51:07 -0300

    initial commit

---
+++ src/main/java/com/cultureclub/cclub/CclubApplication.java
@@ -0,0 +1,13 @@
+package com.cultureclub.cclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cultureclub/cclub/CclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultureclub.cclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
