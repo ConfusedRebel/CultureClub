25fdd7d526a7aec525cf77275400cdb8ae8dcd85
Francisco Javier <fbottazzini@uade.edu.ar>
2025-06-16 22:08:17 -0300

    Refactor SecurityConfig to remove public modifier from filterChain method and add serialVersionUID to UsuarioDuplicateException

--- src/main/java/com/cultureclub/cclub/entity/exceptions/UsuarioDuplicateException.java
+++ src/main/java/com/cultureclub/cclub/entity/exceptions/UsuarioDuplicateException.java
@@ -1,6 +1,9 @@
 package com.cultureclub.cclub.entity.exceptions;
 
+import java.io.Serial;
+
 public class UsuarioDuplicateException extends Exception {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public UsuarioDuplicateException(String message) {
